49cc3d4979b4c55650b4f4289b0ddb2eff829ca6
VANQUYVUONG <vanbin1605@gmail.com>
2024-10-21 13:10:09 +0700

    Add files via upload

--- test.java
+++ test.java
@@ -1,44 +1,26 @@
-package bai3;
-
-import java.util.Scanner;
-
-
-
+package bai2_sinhvien;
+import java.util.*;
 public class test {
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		nganhang tk= new nganhang(0, null, null, 0);
-		tk.nhap_ten_tai_khoan();
-		tk.nhap_so_tai_khoan();
-		tk.nhap_mat_khau();
-		tk.nhap_so_du();
-		int chon;
-        do {
-            System.out.println("Chọn phương án (1: Gửi tiền, 2: Rút tiền,3:Đổi mật khẩu, 0: Thoát): ");
-            chon = sc.nextInt();
 
-            switch (chon) {
-                case 1:
-                    tk.nhaptiengui();
-                    System.out.println("Số dư tài khoản là: " + tk.getSodu());
-                    break;
-                case 2:
-                    tk.nhaptienrut();
-                    System.out.println("Số dư tài khoản là: " + tk.getSodu());
-                    break;
-                
-                case 0:
-                    System.out.println("Thoát chương trình.");
-                    break;
-                default:
-                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
-            }
-        } while (chon != 0); // Lặp cho đến khi người dùng chọn thoát
-
-        sc.close(); // Đóng Scanner
-    }
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        List<sinhvien> danhSach = new ArrayList<>();
+	        System.out.print("Nhap so luong sinh vien: ");
+	        int soLuong = scanner.nextInt();
+	        scanner.nextLine();
+	        for (int i = 0; i < soLuong; i++) {
+	            System.out.println("Nhap thong tin sinh vien thu " + (i + 1) + ":");
+	            sinhvien sv = new sinhvien("", "", "", 0);
+	            sv.nhapThongTin();
+	            danhSach.add(sv);
+	        }
+	        danhSach.sort((sv1, sv2) -> Double.compare(sv2.getDiemTrungBinh(), sv1.getDiemTrungBinh()));
+	        System.out.println("\nDanh sach sinh vien sau khi sap xem theo diem trung binh giam dan:");
+	        for (sinhvien sv : danhSach) {
+	            sv.inThongTin();
+	            System.out.println();
+	        }
+	    
+	}
 
 }
-
-
-
